f55623df2e057b8f74c375c439bce83b7d2f79a2
Rahamath22 <rahamath22104@gmail.com>
2024-04-04 12:19:08 +0530

    Email sending logic

---
+++ EmailSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+
+namespace EmailSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fromAddress = new MailAddress("rahamath22104@gmail.com", "Rahamath");
+            var toAddress = new MailAddress("jaffersha786@gmail.com", "Jaffer");
+            const string fromPassword = "qzyk ddtt mpjz qefw";
+            const string subject = "This mail is form rahamath";
+            const string body = "Hello Daddy";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+    
+   
+    
+
+       }
+    }
+}
