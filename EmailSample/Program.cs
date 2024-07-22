11db981e7edddaba66fb5c10880b5eaaa5565977
Rahamath22 <rahamath22104@gmail.com>
2024-07-22 12:03:05 +0530

    Without Interface a complete email sending program

--- EmailSample/Program.cs
+++ EmailSample/Program.cs
@@ -4,40 +4,100 @@ using System.Linq;
 using System.Text;
 using System.Net.Mail;
 using System.Net;
-
+using System.IO;
+using System.Data.SqlClient;
 namespace EmailSample
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            var fromAddress = new MailAddress("rahamath22104@gmail.com", "Rahamath");
-            var toAddress = new MailAddress("jaffersha786@gmail.com", "Jaffer");
-            const string fromPassword = "qzyk ddtt mpjz qefw";
-            const string subject = "This mail is form rahamath";
-            const string body = "Hello Daddy";
+            Student s = new Student();
+            List<Student> stu = new List<Student>();
+            stu.Add(new Student { Name = "Jaffersha", RollNo = 101, Mailid = "jaffersha786@gmail.com",Message="Hello" });
+            stu.Add(new Student { Name = "Harish", RollNo = 102, Mailid = "harish12ser@gmail.com" ,Message="Hello"});
+            stu.Add(new Student { Name = "Rexalan", RollNo = 103, Mailid = "rexalan1174@gmail.com", Message = "Hello" });
+            stu.Add(new Student { Name = "Nilofar Nisha", RollNo = 104, Mailid = "nilofarnisha464@gmail.com", Message = "Hello" });
 
-            var smtp = new SmtpClient
+            foreach (Student S in stu)
             {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-            };
-            using (var message = new MailMessage(fromAddress, toAddress)
-            {
-                Subject = subject,
-                Body = body
-            })
-            {
-                smtp.Send(message);
+
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(Properties.Settings.Default.Cs))
+                    {
+                        SqlCommand cmd = new SqlCommand();
+                        con.Open();
+                        cmd.Connection = con;
+                        bool Demo = CheckedCondition(S.RollNo);
+                        if (Demo == false)
+                        {
+
+                            bool demo1 = s.Mail(S.Mailid, S.Message, S.Name, S.RollNo);
+                            if (demo1 == true)
+                            {
+                                cmd.CommandText = "insert into Sample values('" + S.RollNo + "','" + 1 + "')";
+                                cmd.ExecuteNonQuery();
+                            }
+                            else
+                            {
+                                cmd.CommandText = "insert into Sample values('" + S.RollNo + "','" + 0 + "')";
+                                cmd.ExecuteNonQuery();
+                            }
+                        }
+                        else
+                        {
+                            cmd.CommandText = "select Checked from Sample where RollNo='" + S.RollNo + "'";
+                            int str = Convert.ToInt32(cmd.ExecuteScalar());
+                            if (str == 0)
+                            {
+
+                                bool demo1 = s.Mail(S.Mailid, S.Message, S.Name, S.RollNo);
+                                if (demo1 == true)
+                                {
+                                    cmd.CommandText = "update Sample set Checked='" + 1 + "' where RollNo='" + S.RollNo + "'";
+                                    cmd.ExecuteNonQuery();
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+                
             }
-    
-   
+        }
+
+        public static bool CheckedCondition(int RN)
+        {
+            
+                    using (SqlConnection con = new SqlConnection(Properties.Settings.Default.Cs))
+                    {
+                        SqlCommand cmd = new SqlCommand();
+                        con.Open();
+                        cmd.Connection = con;
+                        cmd.CommandText = "select count(*) from Sample where RollNo='" + RN + "'";
+                        int str = Convert.ToInt32(cmd.ExecuteScalar());
+                        if (str == 0)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                     }
+              
+        }
+
+     }
+}
+           
+           
+       
     
 
-       }
-    }
-}
+
---
+++ EmailSample/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+
+namespace EmailSample
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        public string Mailid { get; set; }
+        public string Message { get; set; }
+       
+
+        public bool Mail(string mailad, string Message, string name, int rollno)
+        {
+            
+            var fromAddress = new MailAddress("rahamath22104@gmail.com", "Rahamath");
+            var toAddress = new MailAddress(mailad, name);
+            
+            const string fromPassword = "qzyk ddtt mpjz qefw";
+            const string subject = " Message";
+            string body = Message;
+            
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                try
+                {
+                    System.Net.Mail.Attachment attachment;
+                    attachment = new System.Net.Mail.Attachment("D:\\Jaffersha\\" + rollno + ".txt");
+                    message.Attachments.Add(attachment);
+                }
+                catch(Exception ex){}
+                try
+                {
+                    smtp.Send(message);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                    
+                }
+            }
+
+        }
+
+       
+    }
+}
